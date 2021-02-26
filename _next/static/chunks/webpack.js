c8085eb67bfcc558443122ca519aea8cb090d0e8
muratGG <murat.atay@ganygames.com>
2021-02-27 01:19:34 +0300

    Google Analytics

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "091a14f96f4521f7e684";
+/******/ 	var hotCurrentHash = "1787bb345432ea369616";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
